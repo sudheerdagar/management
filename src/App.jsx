e8714752b60e05b4feaa0abf8335a57c884e636c
Anudeepexperiment <anudeep0306@gmail.com>
2023-06-08 19:17:59 +0530

    Added css for these files

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,18 @@ const  App=()=> {
   return (
     
 
+<div className="text-center">
+      <div className="w-full">
+        <h1 className="m-4 text-4xl font-extrabold text-blue-700">Employee form</h1>
+      </div>
 
-    <div>
-    <Employeeform/>
-    <Employees/>
+      <Employeeform />
 
-    
-    
-    
+      <div>
+        <h1 className="m-4 text-4xl font-extrabold text-blue-700">Employee List</h1>
+      </div>
+      <Employees/>
+      {/* Render Employee List Component */}
     </div>
   )
 }
--- src/components/employee-form/employee-form.component.jsx
+++ src/components/employee-form/employee-form.component.jsx
@@ -6,155 +6,139 @@ const defaultFormFields = {
   lname: "",
   age: "",
   number: "",
-  salary:"",
-  account:"",
-  aadhar:""
+  salary: "",
+  account: "",
+  aadhar: ""
 };
 
 export const Employeeform = () => {
-  
-
   const [formFields, setFormFields] = useState(defaultFormFields);
-  const { fname, lname, age,number,salary,account,aadhar} = formFields;
-
+  const { fname, lname, age, number, salary, account, aadhar } = formFields;
 
   const resetformFields = () => {
     setFormFields(defaultFormFields);
   };
 
-
-
-
   const handleChange = (event) => {
-     
-   
-
     let { name, value } = event.target;
-    if(name=="salary"||name==="number"||name==="aadhar"||name==="account")
-    {
-        const numericValue = value.replace(/\D/g, '');
-       
-        value=numericValue;
+    if (name === "salary" || name === "number" || name === "aadhar" || name === "account") {
+      const numericValue = value.replace(/\D/g, '');
+      value = numericValue;
     }
 
     setFormFields({ ...formFields, [name]: value });
   };
 
-  const handleSubmit= async(event)=>{
+  const handleSubmit = async (event) => {
     event.preventDefault();
     console.log("this is handle submit");
-        
-    
+
     await addEmployee(formFields);
-      resetformFields();
-      
-    
-
-  }
-
-
-
+    resetformFields();
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        First name:
-        <input
-          type="text"
-          placeholder="enter-first-name"
-          name="fname"
-          value={fname}
-          required
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br />
+    <form onSubmit={handleSubmit} className="flex justify-center">
+      <div className="max-w-md p-4 bg-white shadow-md rounded-md">
+        <h2 className="text-xl font-bold mb-4">Add Employee</h2>
 
-      <label>
-        Last name:
-        <input
-          type="text"
-          placeholder="enter-last-name"
-          name="lname"
-          value={lname}
-          required
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br />
+        <label className="block">
+          First name:
+          <input
+            type="text"
+            placeholder="Enter first name"
+            name="fname"
+            value={fname}
+            required
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
-      <label>
-        Age:
-        <input
-          type="text"
-          placeholder="enter-age"
-          name="age"
-          value={age}
-          required
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br />
+        <label className="block">
+          Last name:
+          <input
+            type="text"
+            placeholder="Enter last name"
+            name="lname"
+            value={lname}
+            required
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
-      <label>
-        Phone Number:
-        <input
-          type="tel"
-          pattern="[0-9]{10}"
-          value={number}
-          onChange={handleChange}
-          placeholder="Enter your phone number"
-          required
-          name="number"
-        />
-      </label>
-      <br/>
+        <label className="block">
+          Age:
+          <input
+            type="text"
+            placeholder="Enter age"
+            name="age"
+            value={age}
+            required
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
-      <label>
-        Salary:
-        <input
-          type="text"
-          pattern="[0-9]*"
-          placeholder="enter-age"
-          name="salary"
-          value={salary}
-          required
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br/>
-      
-      <label>
-        ACC_NO:
-        <input
-          type="text"
-          pattern="[0-9]*"
-          placeholder="enter-account-number"
-          name="account"
-          value={account}
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br/>
-       
-      <label>
-        AAdhar:
-        <input
-          type="text"
-          pattern="[0-9]*"
-          placeholder="enter-aadhar-number"
-          name="aadhar"
-          value={aadhar}
-          onChange={handleChange}
-        ></input>
-      </label>
-      <br/>
+        <label className="block">
+          Phone Number:
+          <input
+            type="tel"
+            pattern="[0-9]{10}"
+            value={number}
+            onChange={handleChange}
+            placeholder="Enter your phone number"
+            required
+            name="number"
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
+        <label className="block">
+          Salary:
+          <input
+            type="text"
+            pattern="[0-9]*"
+            placeholder="Enter salary"
+            name="salary"
+            value={salary}
+            required
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
-      <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full">
-     add_employee
-</button>
+        <label className="block">
+          ACC_NO:
+          <input
+            type="text"
+            pattern="[0-9]*"
+            placeholder="Enter account number"
+            name="account"
+            value={account}
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
 
+        <label className="block">
+          AAdhar:
+          <input
+            type="text"
+            pattern="[0-9]*"
+            placeholder="Enter Aadhar number"
+            name="aadhar"
+            value={aadhar}
+            onChange={handleChange}
+            className="mt-1 p-1 border border-gray-300 rounded-sm w-full"
+          />
+        </label>
+
+        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full mt-4 w-full">
+          Add Employee
+        </button>
+      </div>
     </form>
   );
 };
